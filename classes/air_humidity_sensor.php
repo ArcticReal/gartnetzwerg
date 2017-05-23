e243d44765f81017ee7b69d36d50410504cf5fd5
dude <dennis.horn@stud.hs-kempten.de>
2017-05-23 15:44:37 +0200

    changed moisture to humidity

--- classes/air_humidity_sensor.php
+++ classes/air_humidity_sensor.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'sensor.php';
-class Air_moisture_sensor extends Sensor{
+class Air_humidity_sensor extends Sensor{
 	public function update(){
 		exec("sudo python3 /home/pi/Adafruit_Python_DHT/sensor_ah.py", $rReturn, $err);
 		
