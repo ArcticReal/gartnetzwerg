c964b650a802c3f021024d056f5ef9d4cd84a76f
dude <dennis.horn@stud.hs-kempten.de>
2017-05-24 11:12:00 +0200

    soil humidity sensor now has no position but there is an extra sensor for waterlogging

--- classes/soil_humidity_sensor.php
+++ classes/soil_humidity_sensor.php
@@ -1,15 +1,8 @@
 <?php
 require_once 'sensor.php';
 class Soil_humidity_sensor extends Sensor{
-	private $position;
+
 	
-	public function set_position($new_position){
-		$this->position = $new_position;
-	}
-	
-	public function get_position(){
-		return $this->position;
-	}
 	
 	public function update(){
 		exec("sudo python3 /home/pi/Adafruit_Python_DHT/sensor_sh.py", $rReturn, $err);
