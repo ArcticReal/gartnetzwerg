ce8ff0625cebfd42c549227d8d13452020edce19
dude <dennis.horn@stud.hs-kempten.de>
2017-06-07 13:26:30 +0200

    ordnerstruktur geändert

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/* [DATABASE] */
-define("HOST", "localhost");
-define("USER", "root");
-define("PASS", "helloplants");
-define("DATABASE", "gartnetzwerg_plant_db");
-?>
--- gui/readme.txt
+++
@@ -1,2 +0,0 @@
-Ansicht aktuell nur gut für unter 940px bildschirmbreite.
-Und ja ich weiß, dass da echt einiges an unnötigem Code drin ist ;)
