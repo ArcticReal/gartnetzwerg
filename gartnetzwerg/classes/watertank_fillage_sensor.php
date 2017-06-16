94e65c954dd9898f450e0582dc1296dba4edca9e
Dennis <dennishorn@hotmail.de>
2017-06-16 21:25:05 +0200

    bugfixing

--- gartnetzwerg/classes/watertank_fillage_sensor.php
+++ gartnetzwerg/classes/watertank_fillage_sensor.php
@@ -19,8 +19,7 @@ class Watertank_fillage_sensor extends Sensor{
 	public function update($mac_address){
 		$path = "sudo python3 /home/pi/gartnetzwerg/sensor_wf.py ".($this->position+3);
 		$cmd = __DIR__."/../connect.sh ".$mac_address." '".$path."'";
-		echo $test = shell_exec($cmd);
-		//$this->set_value(shell_exec($cmd));
+		$this->set_value(shell_exec($cmd));
 		
 	}
 }
