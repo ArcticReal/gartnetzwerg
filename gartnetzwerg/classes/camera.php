1200dc7e4c3fc3e9a0ffb3298b11fd22383dc314
pierrejordan <pierre.l.jordan@stud.hs-kempten.de>
2017-06-18 15:04:33 +0200

    connect.sh rausgenommen

--- gartnetzwerg/classes/camera.php
+++ gartnetzwerg/classes/camera.php
@@ -10,16 +10,17 @@ class Camera extends Sensor{
 	}*/
 	
 	public function take_pic($mac_address,$plant_id,$nickname){
-		$cmd = __DIR__."/../connect.sh ".$mac_address." /home/pi/gartnetzwerg/take_picture.py";
-		shell_exec($cmd);
 		
 		$cmd = __DIR__."/../get_ip_address.sh ".$mac_address;
 		$ip = shell_exec($cmd);
 		
+		$cmd = "ssh -i /home/pi/.ssh/id_rsa pi@".$ip.":/home/pi/gartnetzwerg/take_picture.py";
+		shell_exec($cmd);
+				
 		$cmd = "sudo -u root ". __DIR__."/../fetch_picture.sh ".$ip." /home/pi/Pictures/".$plant_id."_".$nickname;
 		shell_exec($cmd);
 		
-		$cmd = __DIR__."/../connect.sh ".$mac_address." /home/pi/gartnetzwerg/remove_picture.sh";
+		$cmd =  "ssh -i /home/pi/.ssh/id_rsa pi@".$ip.":/home/pi/gartnetzwerg/remove_picture.sh";
 		shell_exec($cmd);
 
 		$cmd = "sudo -u root chown www-data:www-data /home/pi/Pictures/".$plant_id."_".$nickname."/*";
